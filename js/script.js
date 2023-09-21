ad8c1b4d1d9d6452851320642bc10ed1cbd13c86
Aurelia Yee <yee.aurelia@gmail.com>
2023-09-21 09:52:16 -0700

    Assignment 1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,115 @@
-function sayHello() {
-    alert('Hello World!');
-}
+// STEP 1
+// let someMonth
+// function theMonth()
+// let currentMonth
+// let summerMonth
+// let myLibraryFunction
+
+// STEP 2
+// 10.4 				    // numeric literal 
+// 'This is a string' 		// string literal 
+// false 				    // Boolean literal 
+// null 				    // literal null value 
+
+// STEP 3
+// let complexExpression = 1 + 2 * 3;    //complex expression
+// let variableExpression = 'This is ' + fname + 'assignment';    //variable expression
+
+// STEP 4
+// let  strFirstName
+// let strLastName
+// let strAddress
+// let strCity
+// let strState
+// let strZipCode
+// let intAge
+// let strReferralSource
+// let blnMayWeContactYou
+
+// STEP 5
+// let strFirstName = 'Aurelia'
+// let strLastName
+// strLastName = 'Yee'
+// let strCity = 'San Diego', let strZipCode = '92127'
+
+// STEP 6
+// let campSite = 123
+// console.log('We went to Grand Canyon and stayed at campsite ' + campSite + '.')
+
+//STEP 7
+// let isFun = true
+// console.log('It\'s ' + isFun + ' that the trip was fun.')
+
+// let funTimes = 3
+// console.log(funTimes + isFun)
+
+//STEP 8
+// let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."'
+// console.log(someString)
+
+//STEP 9
+// let valueNull = null
+// console.log(valueNull)
+
+// let valueUndefined = undefined
+// console.log(undefined)
+
+//STEP 10
+// console.log(typeof 'test')
+// console.log(typeof 2)
+// console.log(typeof true)
+// console.log(typeof [1, 2, 4])
+// console.log(typeof undeclaredVariable)
+
+//STEP 11
+// alert('Hello ' + 'Aurelia Yee' + ', welcome to the JavaScript class!')
+
+//STEP 12
+// let fullName = 'Aurelia Yee'
+// alert('Hello ' + fullName + ', welcome to the JavaScript class!')
+
+//STEP 13
+// let course = 'Javascript'
+// alert('Hello Aurelia Yee, welcome to the ' + course + ' class!')
+
+//STEP 14
+// let course = 'Javascript'
+// alert('Hello Aurelia Yee.\nWelcome to the ' + course + ' class!')
+
+//STEP 15
+// let fullName = prompt('What is your name?')
+// alert('Hello ' + fullName + ', welcome to the JavaScript class!')
+
+//STEP 16
+// let course = prompt('What class are you taking?')
+// alert('Hello Aurelia Yee, welcome to the ' + course + ' class!')
+
+//STEP 17
+// let x = 10
+// let y = 20
+// console.log(x+y)
+
+//STEP 18
+// let x = 20
+// x += 20
+// console.log(x)
+
+//STEP 19
+// let x = 20
+// x *= 5
+// console.log(x)
+
+//STEP 20
+// let x = 20%3
+// x /= 1
+// console.log(x)
+
+//STEP 21
+// let x = 5
+// let y = 5
+// console.log(x===y)
+
+//STEP 22
+// let x = "10"
+// let y = 10
+// console.log(x===y)
